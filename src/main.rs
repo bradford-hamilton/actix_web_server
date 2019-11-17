426094bfa346dfea4000e16496f23c58880f4cda
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-17 14:05:37 -0700

    add atomic counter in handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
+use std::sync::Mutex;
 
 // One way to implement a responder
 fn index() -> impl Responder {
@@ -11,10 +12,28 @@ fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello yourself")
 }
 
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
+}
+
+fn counter_handler(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    
+    *counter += 1; // Access counter inside MutexGuard
+
+    format!("Request number: {}", counter)
+}
+
 fn main() {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter{
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || { // Move counter into closure
         App::new()
+            .register_data(counter.clone())
             .route("/", web::get().to(index)) // Register the index responder function
+            .route("/counter", web::get().to(counter_handler)) // Register route /counter to counter
             .service(hello) // Register the hello using attribute macro
     })
     .bind("127.0.0.1:3000")
